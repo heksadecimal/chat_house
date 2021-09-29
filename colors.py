6095fb03614ffa00593e369687c5ccb81643b783
heksadecimal <heksadecimal54@gmail.com>
2021-09-29 21:21:24 +0530

    added flair

---
+++ colors.py
@@ -0,0 +1,36 @@
+class color:
+    """Colors class:
+    Reset all colors with colors.reset
+    Two subclasses fg for foreground and bg for background.
+    Use as colors.subclass.colorname.
+    i.e. colors.fg.red or colors.bg.green
+    Also, the generic bold, disable, underline, reverse, strikethrough,
+    and invisible work with the main class
+    i.e. colors.bold
+    """
+
+    class style:
+        reset = "\033[0m"
+        bold = "\033[01m"
+        disable = "\033[02m"
+        underline = "\033[04m"
+        reverse = "\033[07m"
+        strikethrough = "\033[09m"
+        invisible = "\033[08m"
+
+    class fg:
+        black = "\033[30m"
+        red = "\033[31m"
+        green = "\033[32m"
+        orange = "\033[33m"
+        blue = "\033[34m"
+        purple = "\033[35m"
+        cyan = "\033[36m"
+        lightgrey = "\033[37m"
+        darkgrey = "\033[90m"
+        lightred = "\033[91m"
+        lightgreen = "\033[92m"
+        yellow = "\033[93m"
+        lightblue = "\033[94m"
+        pink = "\033[95m"
+        lightcyan = "\033[96m"
